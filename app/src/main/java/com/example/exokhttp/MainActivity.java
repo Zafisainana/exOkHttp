0eac739b5e80b5d9cab533a7c8de9d5695a2868b
zafisainana randriamanantenasoa <zafisainana.randria@outlook.fr>
2020-04-03 16:09:17 +0200

    recuperation du text et bouton

--- app/src/main/java/com/example/exokhttp/MainActivity.java
+++ app/src/main/java/com/example/exokhttp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.exokhttp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.textclassifier.TextLinks;
+import android.widget.Button;
+import android.widget.TextView;
 
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -12,11 +15,22 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtReponse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    txtReponse = findViewById(R.id.txtReponse);
+    Button btnServiceWeb = findViewById(R.id.btnServiceWeb);
+
+    btnServiceWeb.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            callServiceWeb();
+        }
+    });
 
     }
 
@@ -40,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
             if(response.isSuccessful()){
                 String retour = response.body().string();
+                txtReponse.setText(retour);
             }
         }
         catch (Exception ex)
